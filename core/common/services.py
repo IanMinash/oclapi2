b0cc6f489ca43486e4c53888fd0e4be11c38aef5
Ian Minara <minaraian@gmail.com>
2023-03-22 16:27:23 +0300

    Try file size

--- core/common/services.py
+++ core/common/services.py
@@ -365,7 +365,7 @@ class MinioExportService:
                 settings.AWS_STORAGE_BUCKET_NAME,
                 file_path,
                 file_content,
-                length=-1,
+                length=file_content.getbuffer().nbytes,
                 metadata={"ACL": "public-read"},
             )
         except S3Error:  # pragma: no cover
